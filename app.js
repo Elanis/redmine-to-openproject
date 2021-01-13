70cd7cb55e0c5b82eed92fd1534e92ad1d53bdf8
Elanis <elanis@hotmail.com>
2021-01-13 18:16:40 +0100

    Migrate "versions" table

--- app.js
+++ app.js
@@ -16,6 +16,75 @@ const openProjectCredentials = {
 	port: 5432,
 };
 
+/**
+TABLES TO MIGRATE:
+[ ] "ar_internal_metadata"
+[ ] "attachments"
+[ ] "auth_sources"
+[ ] "boards"
+[ ] "changes"
+[ ] "changeset_parents"
+[ ] "changesets"
+[ ] "changesets_issues"
+[ ] "comments"
+[ ] "custom_field_enumerations"
+[ ] "custom_fields"
+[ ] "custom_fields_projects"
+[ ] "custom_fields_roles"
+[ ] "custom_fields_trackers"
+[ ] "custom_values"
+[ ] "documents"
+[ ] "email_addresses"
+[ ] "enabled_modules"
+[ ] "enumerations"
+[ ] "groups_users"
+[ ] "imports"
+[ ] "issue_categories"
+[ ] "issue_relations"
+[ ] "issue_statuses"
+[ ] "import_items"
+[ ] "issues"
+[ ] "journal_details"
+[ ] "journals"
+[ ] "member_roles"
+[ ] "members"
+[ ] "messages"
+[ ] "news"
+[ ] "open_id_authentication_associations"
+[ ] "open_id_authentication_nonces"
+[ ] "messenger_settings"
+[X] "projects"
+[ ] "projects_trackers"
+[ ] "queries"
+[ ] "queries_roles"
+[ ] "repositories"
+[ ] "roles"
+[ ] "roles_managed_roles"
+[ ] "schema_migrations"
+[ ] "settings"
+[ ] "tokens"
+[ ] "user_preferences"
+[ ] "wiki_content_versions"
+[ ] "users"
+[X] "versions"
+[ ] "watchers"
+[ ] "wiki_contents"
+[ ] "time_entries"
+[ ] "wiki_pages"
+[ ] "trackers"
+[ ] "wiki_redirects"
+[ ] "wikis"
+[ ] "workflows"
+[ ] "agile_data"
+[ ] "agile_colors"
+[ ] "agile_sprints"
+[ ] "checklist_template_categories"
+[ ] "checklist_templates"
+[ ] "checklists"
+[ ] "tags"
+[ ] "taggings"
+**/
+
 // CODE
 const redminePool = new pg.Pool(redmineCredentials);
 const openProjectPool = new pg.Pool(openProjectCredentials);
@@ -68,20 +137,60 @@ function transformProjectObject(project) {
 		lft: project.lft,
 		rgt: project.rgt,
 		templated: false,
+		// ?: project.homepage,
+		// ?: project.status,
+		// ?: project.inherit_members,
+		// ?: project.default_version_id,
+		// ?: project.default_assigned_to_id,
+	};
+}
+
+function transformVersionObject(version) {
+	return {
+		id: version.id,
+		project_id: version.project_id,
+		name: version.name,
+		description: version.description,
+		effective_date: version.effective_date,
+		created_at: version.created_on,
+		updated_at: version.updated_on,
+		wiki_page_title: version.wiki_page_title,
+		status: version.status,
+		sharing: version.sharing,
+		// ?: version.start_date,
 	};
 }
 
 (async () => {
-	// Projects
-	const redmineProjects = (await redminePool.query('SELECT * FROM projects')).rows;
+	/**
+	 * Clean db
+	 */
+	console.log('Cleaning versions');
+	await openProjectPool.query('DELETE FROM versions');
 
 	console.log('Cleaning projects');
 	await openProjectPool.query('DELETE FROM projects');
 
-	for(const project of redmineProjects) {
+	/**
+	 * Migrate data
+	 */
+
+	// Projects
+	const projectList = (await redminePool.query('SELECT * FROM projects')).rows;
+
+	for(const project of projectList) {
 		console.log('Inserting project ' + project.name);
 
 		const [query, values] = buildInsertQuery('projects', transformProjectObject(project));
 		await openProjectPool.query(query, values);
+
+		// Versions
+		const versionList = (await redminePool.query('SELECT * FROM versions WHERE project_id = $1', [project.id])).rows;
+		for(const version of versionList) {
+			console.log('  Inserting version ' + version.name);
+
+			const [query, values] = buildInsertQuery('versions', transformVersionObject(version));
+			await openProjectPool.query(query, values);
+		}
 	}
 })();
